193f6e4a84e9a57708eeb8611fb666973e9b3b7b
Karthik-TK <karthiktk16@gmail.com>
2021-05-27 19:17:55 +0530

    modified db fields

--- server/fifa/models.py
+++ server/fifa/models.py
@@ -3,10 +3,16 @@ from django.db import models
 # Create your models here.
 
 class Fifa(models.Model):
-    title = models.CharField(max_length=120)
-    description = models.TextField(max_length=300)
-    # age = models.IntegerField()
-    completed = models.BooleanField(default=False)
+
+    player_id = models.IntegerField()
+    name = models.CharField(max_length=120)
+    nationality = models.CharField(max_length=120, default='SOME STRING')
+    position = models.CharField(max_length=120, default='SOME STRING')
+    overall = models.IntegerField()
+    age = models.IntegerField()
+    hits = models.IntegerField()
+    potential = models.IntegerField()
+    team = models.CharField(max_length=120)
 
     def _str_(self):
         return self.title
